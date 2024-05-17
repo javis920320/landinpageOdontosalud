323d2539450062d902ff6589f74c7506f4081fc2
javis920320 <javis9203@gmail.com>
2024-05-17 08:27:51 -0500

    modificacion para permitir despliegue , conflicto con archivo env

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import { api_google } from './src/env.d.ts';
+//import { api_google } from './src/env';
 
 export default defineConfig({
   integrations: [
     tailwind(), // Integración de Tailwind CSS
     {
       name: 'google-maps', // Nombre de la integración de Google Maps
-      async script({ loadInlineScript }) {
+      /* async script({ loadInlineScript }) {
         await loadInlineScript(api_google);
-      },
+      }, */
     },
   ],
 });
